bef80e92be6619c0884e35cf857142a51eec783e
Yury Velikanau <yury.velikanau@gmail.com>
2018-04-16 00:41:39 -0700

    Use full module name.

--- lib/tasks/scheduler.ex
+++ lib/tasks/scheduler.ex
@@ -5,6 +5,6 @@ defmodule Mix.Tasks.MixExq.Scheduler do
 
   def run(_args) do
     {:ok, _} = Exq.Enqueuer.start_link([])
-    Exq.Enqueuer.enqueue(Exq.Enqueuer, "default", Workers.MyWorker, [1])
+    Exq.Enqueuer.enqueue(Exq.Enqueuer, "default", MixExq.Workers.MyWorker, [1])
   end
 end
